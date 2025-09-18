f75dafbc5f47edc5d169bd54ebb7e8c312c3d01b
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-09-18 16:31:04 +0600

    sendResponse utility function created

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,29 @@
+import { Response } from "express";
+
+
+interface IMeta {
+  page?: number;
+  limit?: number;
+  totalPage?: number;
+  total: number;
+}
+
+interface IResponse<T> {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  meta?: IMeta;
+  data: T;
+}
+
+export const sendResponse = <T>(res: Response, data: IResponse<T>) =>{
+  res.status(data.statusCode).json(
+    {
+      statusCode: data.statusCode,
+      success: data.success,
+      message: data.message,
+      meta: data.meta,
+      data: data.data
+    }
+  )
+}
