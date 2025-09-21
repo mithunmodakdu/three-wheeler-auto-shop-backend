8866c78186216b41db351cfc8ba25b61e74aa817
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-09-21 15:27:11 +0600

    IErrorSources, IGenericErrorResponse interfaces created

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { userControllers } from "./user.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { createUserZodSchema } from "./user.validation";
 
 
 const router = Router();
 
-router.post("/register", userControllers.createUser);
+router.post("/register",
+  validateRequest(createUserZodSchema),
+  userControllers.createUser
+);
 
 export const UserRoutes = router; 
