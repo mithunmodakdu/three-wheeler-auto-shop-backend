8455e63597b27901565659da0dc37eeef4111e32
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-09-24 15:51:53 +0600

    credentialsLogin logics in AuthService completed

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,7 +1,27 @@
+import AppError from "../../errorHelpers/appError";
 import { IUser } from "../user/user.interface";
+import { User } from "../user/user.model";
+import httpStatusCodes from "http-status-codes";
+import bcryptjs from "bcryptjs";
 
 const credentialsLogin = async(payload: Partial<IUser>) =>{
-  console.log(payload)
+  const {email, password} = payload;
+
+  const isUserExist = await User.findOne({email});
+
+  if(!isUserExist){
+    throw new AppError(httpStatusCodes.BAD_REQUEST, "Email does not exist.")
+  }
+
+  const isPasswordMatched = await bcryptjs.compare(password as string, isUserExist.password as string);
+  
+  if(!isPasswordMatched){
+    throw new AppError(httpStatusCodes.BAD_REQUEST, "Password does not match.")
+  }
+
+  return {
+    email: isUserExist.email
+  }
 }
 
 export const AuthServices = {
