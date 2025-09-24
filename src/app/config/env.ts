b343992bb8316e4a21d8e6d4bcbe138c3c1c3427
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-09-24 16:24:11 +0600

    accessToken created using jwt sign

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -7,6 +7,7 @@ interface IEnvVariables {
   DB_URL: string;
   NODE_ENV: string;
   BCRYPT_SALT_ROUND: string;
+  JWT_WEB_SECRET: string;
 }
 
 const loadEnvVariables = (): IEnvVariables => {
@@ -15,6 +16,7 @@ const loadEnvVariables = (): IEnvVariables => {
     "DB_URL",
     "NODE_ENV",
     "BCRYPT_SALT_ROUND",
+    "JWT_WEB_SECRET"
   ];
 
   requiredEnvVariables.forEach((key) => {
@@ -28,6 +30,7 @@ const loadEnvVariables = (): IEnvVariables => {
     DB_URL: process.env.DB_URL as string,
     NODE_ENV: process.env.NODE_ENV as string,
     BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+    JWT_WEB_SECRET: process.env.JWT_WEB_SECRET as string
   };
 };
 
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import httpStatusCodes from "http-status-codes";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { envVars } from "../../config/env";
 
 const credentialsLogin = async(payload: Partial<IUser>) =>{
   const {email, password} = payload;
@@ -19,8 +21,17 @@ const credentialsLogin = async(payload: Partial<IUser>) =>{
     throw new AppError(httpStatusCodes.BAD_REQUEST, "Password does not match.")
   }
 
+  const jwtPayload = {
+    userId: isUserExist._id,
+    email: isUserExist.email,
+    role: isUserExist.role
+  }
+
+  const accessToken = jwt.sign(jwtPayload, envVars.JWT_WEB_SECRET, {expiresIn: "1d"});
+
   return {
-    email: isUserExist.email
+    email: isUserExist.email,
+    accessToken
   }
 }
 
